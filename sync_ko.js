ba31dcb43283016c5433a0f69bc74188e56d24fb
juuuno-coder <juuuno1116@gmail.com>
2026-01-17 01:47:10 +0900

    style: UI refinements (Hero, Partners, Product Grid) and Mobile tweaks

---
+++ sync_ko.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcPath = 'd:\\코딩\\EMT\\ko\\index.html';
+const destEmtKorea = 'd:\\코딩\\EMT\\emt-korea\\EMT.html';
+const destGlobalKo = 'd:\\코딩\\EMT\\emt-global\\ko\\index.html';
+
+try {
+    const content = fs.readFileSync(srcPath, 'utf8');
+
+    // 1. emt-korea
+    fs.writeFileSync(destEmtKorea, content, 'utf8');
+    console.log('Updated emt-korea');
+
+    // 2. global ko
+    const contentGlobal = content.replace(/\.\.\/assets\//g, '../../assets/');
+    fs.writeFileSync(destGlobalKo, contentGlobal, 'utf8');
+    console.log('Updated global ko');
+    
+} catch (e) {
+    console.error(e);
+}
