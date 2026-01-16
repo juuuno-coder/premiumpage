374a1ea9bf823eab0aec5a8354670d00eb5798e5
juuuno-coder <juuuno1116@gmail.com>
2026-01-17 07:58:39 +0900

    style: Final UI polish (Partners layout, Diagram alignment, Scroll fix)

--- sync_ko.js
+++ sync_ko.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 
 const srcPath = 'd:\\코딩\\EMT\\ko\\index.html';
 const destEmtKorea = 'd:\\코딩\\EMT\\emt-korea\\EMT.html';
@@ -7,16 +6,10 @@ const destGlobalKo = 'd:\\코딩\\EMT\\emt-global\\ko\\index.html';
 
 try {
     const content = fs.readFileSync(srcPath, 'utf8');
-
-    // 1. emt-korea
     fs.writeFileSync(destEmtKorea, content, 'utf8');
-    console.log('Updated emt-korea');
-
-    // 2. global ko
     const contentGlobal = content.replace(/\.\.\/assets\//g, '../../assets/');
     fs.writeFileSync(destGlobalKo, contentGlobal, 'utf8');
-    console.log('Updated global ko');
-    
+    console.log('Done');
 } catch (e) {
     console.error(e);
 }
